66aef8f8844fae015b3740faeb02ab58704ca357
beldrevo <beldrevo.by@gmail.com>
2017-10-19 20:23:23 -0700

    updated script tag

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 
 USE bamazon;
@@ -5,17 +6,17 @@ USE bamazon;
 CREATE TABLE products (
     id int PRIMARY KEY AUTO_INCREMENT not null,
     product_name VARCHAR(50) not null,
-  	department_name VARCHAR(50) not null,
+    department_name VARCHAR(50) not null,
     price int not null,
     stock_quantity int(10) not null,
-    product_sales int not null
+    product_sales int null
 );
 
 CREATE TABLE departments (
     department_id int PRIMARY KEY AUTO_INCREMENT not null,
     department_name VARCHAR(50) not null,
     over_head_costs int not null,
-    product_sales int not null
+    product_sales int null
 );
     
     INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("iMAC 21.5IN 2.3GHZ 1TB", "electronics", 1099, 4);
@@ -31,7 +32,7 @@ CREATE TABLE departments (
     INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("MB 12IN 1.2GHz 256Gb", "refurbished", 1299, 3);
     INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("MB 12IN 1.9GHz 512Gb", "refurbished", 1299, 2);
     
-  	INSERT INTO departments (department_name, over_head_costs, product_sales) VALUES ("sales", 30000, 0);
+    INSERT INTO departments (department_name, over_head_costs, product_sales) VALUES ("sales", 30000, 0);
     INSERT INTO departments (department_name, over_head_costs, product_sales) VALUES ("electronics", 30000, 0);
     INSERT INTO departments (department_name, over_head_costs, product_sales) VALUES ("refurbished", 7000, 0);
     
